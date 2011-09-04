b1b440b805200d011dff53da2efbb9d42a6228ba
Tim Condit <timc@envisioninc.com>
2011-09-03 19:13:14 -0700

    Move installed apps logic into method

--- Extractor/Clyde.cs
+++ Extractor/Clyde.cs
@@ -48,46 +48,7 @@ namespace PatchTool
             Extractor e = new Extractor();
 
             IEnumerable<string> patchableApps = new List<string> { "Server", "ChannelManager", "WMWrapperService", "Tools" };
-            IEnumerator<string> pApps = patchableApps.GetEnumerator();
-            IDictionary<string, string> installedApps = new Dictionary<string, string>();
-
-            while (pApps.MoveNext())
-            {
-                try
-                {
-                    // Create a new RegistryKey instance every time, or the value detection fails (don't know why)
-                    string subKey = @"SOFTWARE\Envision\Click2Coach\" + pApps.Current;
-                    RegistryKey rk = Registry.LocalMachine.OpenSubKey(subKey);
-
-                    // Registry.GetValue() throws an ArgumentException if the value is not found.  It's an error if the
-                    // "null" is passed to installedApps, but the method requires a default value.
-                    string installPath = Registry.GetValue(rk.ToString(), "InstallPath", "null").ToString();
-                    installedApps.Add(pApps.Current, installPath);
-                    logger.Info("InstallPath found for {0}", pApps.Current);
-                    rk.Close();
-                }
-                catch (NullReferenceException)
-                {
-                    logger.Info("InstallPath not found for {0}", pApps.Current);
-                }
-                catch (ArgumentException)
-                {
-                    logger.Info("InstallPath not found for {0}", pApps.Current);
-                }
-            }
-
-            if (installedApps.Count == 0)
-            {
-                logger.Warn("No Envision applications were found on this machine!");
-            }
-            else
-            {
-                logger.Info("Found {0} Envision applications:", installedApps.Count);
-                foreach (KeyValuePair<string, string> item in installedApps)
-                {
-                    logger.Info("{0} installed at \"{1}\"", item.Key, item.Value);
-                }
-            }
+            IDictionary<string, string> installedApps = getInstalledApps(patchableApps);
 
             // TC: read the APPDIR from the registry
             RegistryKey hklm = Registry.LocalMachine;
@@ -133,14 +94,48 @@ namespace PatchTool
             Console.ReadLine();
         }
 
-        //private static string PathName(string _name)
-        //{
-        //    get
-        //    {
-        //        RegistryKey rk = Registry.LocalMachine;
-        //        rk.OpenSubKey(@"SOFTWARE\Envision\Click2Coach\ChannelManager");
-        //        return (string)rk.GetValue("InstallPath");
-        //    }
-        //}
+        private static IDictionary<string, string> getInstalledApps(IEnumerable<string> patchableApps)
+        {
+            IDictionary<string, string> installedApps = new Dictionary<string, string>();
+            IEnumerator<string> pApps = patchableApps.GetEnumerator();
+            while (pApps.MoveNext())
+            {
+                try
+                {
+                    // Create a new RegistryKey instance every time, or the value detection fails (don't know why)
+                    string subKey = @"SOFTWARE\Envision\Click2Coach\" + pApps.Current;
+                    RegistryKey rk = Registry.LocalMachine.OpenSubKey(subKey);
+
+                    // Registry.GetValue() throws an ArgumentException if the value is not found.  It's an error if the
+                    // "null" is passed to installedApps, but the method requires a default value.
+                    string installPath = Registry.GetValue(rk.ToString(), "InstallPath", "null").ToString();
+                    installedApps.Add(pApps.Current, installPath);
+                    logger.Info("InstallPath found for {0}", pApps.Current);
+                    rk.Close();
+                }
+                catch (NullReferenceException)
+                {
+                    logger.Info("InstallPath not found for {0}", pApps.Current);
+                }
+                catch (ArgumentException)
+                {
+                    logger.Info("InstallPath not found for {0}", pApps.Current);
+                }
+            }
+
+            if (installedApps.Count == 0)
+            {
+                logger.Warn("No Envision applications were found on this machine!");
+            }
+            else
+            {
+                logger.Info("Found {0} Envision applications:", installedApps.Count);
+                foreach (KeyValuePair<string, string> item in installedApps)
+                {
+                    logger.Info("{0} installed at \"{1}\"", item.Key, item.Value);
+                }
+            }
+            return installedApps;
+        }
     }
 }
