5f9dc759ff2289f116a8f1d81afa3a9acc742e6c
Tim Condit <timc@envisioninc.com>
2011-09-23 22:04:10 -0700

    Add version and copyright info from server.dll
    The reference paths will have to be updated when moving to SVN.

--- PatchLib/Properties/AssemblyInfo.cs
+++ PatchLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using com.et.versioninfo;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PatchLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany(VersionInfo.COMPANY)]
 [assembly: AssemblyProduct("PatchLib")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright(VersionInfo.COPYRIGHT)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(VersionInfo.PRODUCT_VERSION)]
+[assembly: AssemblyFileVersion(VersionInfo.PRODUCT_VERSION)]
